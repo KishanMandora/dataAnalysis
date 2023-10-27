b828c5585be20b08b07cbb48f7c1d0bcb68f1671
Kishan Mandora <kishanmandora7@gmail.com>
2023-10-27 23:36:54 +0530

    refactor: cover response failed and empty response edge cases

--- app.js
+++ app.js
@@ -2,42 +2,69 @@ const email = "kishanmandora7@gmail.com";
 const GET_URL = "https://one00x-data-analysis.onrender.com/assignment?email=";
 const POST_URL = "https://one00x-data-analysis.onrender.com/assignment";
 
-async function getData(email) {
+async function getData(email, failCount = 5) {
   try {
     const response = await fetch(`${GET_URL}${email}`);
-    const assignmentId = response.headers.get("x-assignment-id");
-    const data = await response.json();
 
-    return [data, assignmentId];
+    if (!response.ok) {
+      throw new Error("Error in fetching data, please try again");
+    } else {
+      const assignmentId = response.headers.get("x-assignment-id");
+      const data = await response.json();
+
+      return [data, assignmentId];
+    }
   } catch (err) {
-    console.log(err);
-    return getData();
+    if (failCount <= 0) {
+      console.log(
+        "Failed to fetch data, please try again later after sometime"
+      );
+      return [[], null];
+    } else {
+      console.log(err);
+      failCount--;
+      return getData(email, failCount);
+    }
   }
 }
 
 function getBuzzWord(data) {
-  const buzzWords = {};
+  if (data.length === 0) {
+    return [];
+  }
+
+  const buzzWordsFrequency = {};
 
   data.forEach((item) => {
-    buzzWords[item] ? (buzzWords[item] += 1) : (buzzWords[item] = 1);
+    buzzWordsFrequency[item]
+      ? (buzzWordsFrequency[item] += 1)
+      : (buzzWordsFrequency[item] = 1);
   });
 
-  const maxCount = Object.values(buzzWords).reduce((prev, curr) =>
+  const maxCount = Object.values(buzzWordsFrequency).reduce((prev, curr) =>
     curr > prev ? curr : prev
   );
 
-  const buzzWordsArr = Object.keys(buzzWords).filter(
-    (key) => buzzWords[key] === maxCount
+  const topBuzzWords = Object.keys(buzzWordsFrequency).filter(
+    (key) => buzzWordsFrequency[key] === maxCount
   );
 
-  return buzzWordsArr;
+  return topBuzzWords;
 }
 
-async function submitBuzzWord(email, assignmentId, buzzWords) {
+async function submitBuzzWord(email, assignmentId, topBuzzWords) {
+  if (!assignmentId) {
+    return [false, "assignment Id not found, please try again"];
+  }
+
+  if (topBuzzWords.length === 0) {
+    return [false, "No buzz word found"];
+  }
+
   let index = 0;
   let isCorrect = false;
 
-  while (!isCorrect && index < buzzWords.length) {
+  while (!isCorrect && index < topBuzzWords.length) {
     try {
       const response = await fetch(POST_URL, {
         method: "POST",
@@ -46,35 +73,50 @@ async function submitBuzzWord(email, assignmentId, buzzWords) {
         },
         body: JSON.stringify({
           email: email,
-          answer: buzzWords[index],
+          answer: topBuzzWords[index],
           assignment_id: assignmentId,
         }),
       });
 
-      const data = await response.json();
+      if (!response.ok) {
+        throw new Error("Error in submitting data, please try again");
+      } else {
+        const data = await response.json();
 
-      if (data.result === "submitted_correct") {
-        isCorrect = true;
-        break;
+        if (data.result === "submitted_correct") {
+          isCorrect = true;
+          break;
+        }
       }
     } catch (err) {
+      index--;
       console.log(err);
     }
 
     index++;
   }
 
-  return isCorrect ? buzzWords[index] : "No correct answer found";
+  return isCorrect
+    ? [true, topBuzzWords[index]]
+    : [false, "No correct answer found"];
 }
 
 async function completeDataAnalysis(email) {
   const [data, assignmentId] = await getData(email);
-  const buzzWords = getBuzzWord(data);
-  const buzzWord = await submitBuzzWord(email, assignmentId, buzzWords);
-
-  console.log(
-    `The buzz word for ${email} with assignmentId ${assignmentId} is ${buzzWord}`
+  const topBuzzWords = getBuzzWord(data);
+  const [validResult, msgOrBuzzWord] = await submitBuzzWord(
+    email,
+    assignmentId,
+    topBuzzWords
   );
+
+  if (validResult) {
+    console.log(
+      `Correct answer for ${email} with id of ${assignmentId} is ${msgOrBuzzWord}`
+    );
+  } else {
+    console.log(msgOrBuzzWord);
+  }
 }
 
 completeDataAnalysis(email);
