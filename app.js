34d524382ec6116576ec8cd97d9581d8ad4e5ba9
Kishan Mandora <kishanmandora7@gmail.com>
2023-10-27 08:09:20 +0530

    feat: find buzzwords

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 const email = "kishanmandora7@gmail.com";
-const GET_URL = `https://one00x-data-analysis.onrender.com/assignment?email=${email}`;
+const GET_URL = "https://one00x-data-analysis.onrender.com/assignment?email=";
+const POST_URL = "https://one00x-data-analysis.onrender.com/assignment";
 
-let data, assignmentId;
-
-async function getData() {
+async function getData(email) {
   try {
-    const response = await fetch(GET_URL);
-    assignmentId = response.headers.get("x-assignment-id");
-    data = await response.json();
+    const response = await fetch(`${GET_URL}${email}`);
+    const assignmentId = response.headers.get("x-assignment-id");
+    const data = await response.json();
+
+    return [data, assignmentId];
   } catch (err) {
     console.log(err);
+    return getData();
   }
-
-  return [data, assignmentId];
 }
 
-getData();
+function getBuzzWord(data) {
+  const buzzWords = {};
+
+  data.forEach((item) => {
+    buzzWords[item] ? (buzzWords[item] += 1) : (buzzWords[item] = 1);
+  });
+
+  const maxCount = Object.values(buzzWords).reduce((prev, curr) =>
+    curr > prev ? curr : prev
+  );
+
+  const buzzWordsArr = Object.keys(buzzWords).filter(
+    (key) => buzzWords[key] === maxCount
+  );
+
+  console.log("buzzWordsArr", buzzWordsArr);
+
+  return buzzWordsArr;
+}
+
+async function completeDataAnalysis(email) {
+  const [data, assignmentId] = await getData(email);
+  const buzzWords = getBuzzWord(data);
+
+  console.log("buzzWords", buzzWords);
+}
+
+completeDataAnalysis(email);
